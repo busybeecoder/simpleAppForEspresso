206fdb3724c180ff6c1dbfe7612fa8961fa604c2
bigcoachpotato <48162523+bigcoachpotato@users.noreply.github.com>
2022-03-13 18:51:41 +0300

    Allure integration

--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EssTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/EssTest.java
@@ -5,25 +5,75 @@ import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import android.Manifest;
+import android.os.Environment;
+
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
+
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
 
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
 import org.junit.runner.RunWith;
 
-@RunWith(AndroidJUnit4.class)
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class EssTest {
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, org.junit.runner.Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+    };
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+
     @Rule
     public ActivityTestRule<MainActivity> activityTestRule = new ActivityTestRule<>(MainActivity.class);
 
     @Test
     public void goodTest() {
+        Allure.feature("goodTest");
         onView(withId(R.id.text_home)).check(matches(withText("This is home fragment")));
     }
 
     @Test
     public void badTest() {
+        Allure.feature("badTest");
         onView(withId(R.id.text_home)).check(matches(withText("ababacababacbacbcabacbab")));
+    }
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/screenshots/");
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
--- app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IntentAndListTest.java
+++ app/src/androidTest/java/ru/kkuzmichev/simpleappforespresso/IntentAndListTest.java
@@ -10,25 +10,59 @@ import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withContentDescription;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static androidx.test.platform.app.InstrumentationRegistry.getInstrumentation;
 
+import android.Manifest;
 import android.content.Intent;
+import android.os.Environment;
 
 import androidx.test.espresso.IdlingRegistry;
 import androidx.test.espresso.ViewInteraction;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.rule.ActivityTestRule;
+import androidx.test.rule.GrantPermissionRule;
+import androidx.test.uiautomator.UiDevice;
+
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+import io.qameta.allure.kotlin.Allure;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
 import org.junit.runner.RunWith;
 
 import static org.hamcrest.CoreMatchers.allOf;
 
-@RunWith(AndroidJUnit4.class)
+import io.qameta.allure.android.runners.AllureAndroidJUnit4;
+
+@RunWith(AllureAndroidJUnit4.class)
 public class IntentAndListTest {
+
+    @Rule
+    public TestWatcher watcher = new TestWatcher() {
+        @Override
+        protected void failed(Throwable e, org.junit.runner.Description description) {
+            String className = description.getClassName();
+            className = className.substring(className.lastIndexOf('.') + 1);
+            String methodName = description.getMethodName();
+            takeScreenshot(className + "#" + methodName);
+        }
+    };
+
+    @Rule
+    public GrantPermissionRule mRuntimePermissionRule = GrantPermissionRule
+            .grant(
+                    Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+            );
+
 
     @Before //Выполняется перед тестами
     public void registerIdlingResources() { //Подключаемся к “счетчику”
@@ -41,6 +75,7 @@ public class IntentAndListTest {
 
     @Test
     public void checkIntent() {
+        Allure.feature("checkIntent");
         String url = "https://google.com";
         ViewInteraction options = onView(
                 withContentDescription("More options")
@@ -58,6 +93,7 @@ public class IntentAndListTest {
 
     @Test
     public void checkList() {
+        Allure.feature("checkList");
         ViewInteraction navigation = onView(
                 withContentDescription("Open navigation drawer")
         );
@@ -79,5 +115,20 @@ public class IntentAndListTest {
     @After // Выполняется после тестов
     public void unregisterIdlingResources() { //Отключаемся от “счетчика”
         IdlingRegistry.getInstance().unregister(IdlingResources.idlingResource);
+    }
+
+    private void takeScreenshot(String name) {
+        File path = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/screenshots/");
+        if (!path.exists()) {
+            path.mkdirs();
+        }
+        UiDevice device = UiDevice.getInstance(getInstrumentation());
+        String filename = name + ".png";
+        device.takeScreenshot(new File(path, filename));
+        try {
+            Allure.attachment(filename, new FileInputStream(new File(path, filename)));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
